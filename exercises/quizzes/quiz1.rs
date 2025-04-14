be74f95caad6fecee7977e9fa778592c8533b5c4
Lucas Siqueira <siqueiraq@gmail.com>
2025-04-13 23:09:14 -0300

    solve quiz1

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -10,7 +10,13 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(n: i32) -> i32 {
+    if n > 40 {
+        return n * 1;
+    } else {
+        return n * 2;
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
